383d6f78478c8b9b2fd59cbceccadb6d8ad25485
Ígor Pedro Leitão Mascarenhas Martins <iplmmartins@ibm.com>
2021-07-26 01:11:07 -0300

    Upload information file to database

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,61 @@
 import { Request, Response, Router } from 'express';
 import multer from 'multer';
+import { Readable } from 'stream';
+import readLine from 'readline';
+import { client } from './database/client';
 
 // Multer can sometimes options
 const multerConfig = multer();
 
 const router = Router();
 
-router.post('/products', multerConfig.single('file'), (request: Request, response: Response) => {
-    console.log(request.file);
-    return response.send();
+interface Product {
+    code_bar: string;
+    description: string;
+    price: number;
+    quantity: number;
+}
+
+router.post('/products', multerConfig.single('file'),
+    async (request: Request, response: Response) => {
+    const buffer = request.file?.buffer;
+
+    const readableFile = new Readable();
+    readableFile.push(buffer);
+    readableFile.push(null);
+
+    const productLine = readLine.createInterface({
+        input: readableFile
+    });
+
+    const products: Product[] = [];
+
+    for await (let line of productLine) {
+        const productLineSplit = line.split(',');
+
+        products.push({
+            code_bar: productLineSplit[0],
+            description: productLineSplit[1],
+            price: Number(productLineSplit[2]),
+            quantity: Number(productLineSplit[3])
+        });
+    }
+
+    for await (let { code_bar, description, price, quantity } of products) {
+        await client.products.create({
+            data: {
+                code_bar,
+                description,
+                price,
+                quantity
+            }
+        });
+    }
+
+    return response.json({
+        'Status': 200,
+        'Message': 'Success Save'
+    });
 });
 
 export {
